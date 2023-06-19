b0fc047bac7b231783b916ffa7c1eb7f6585541d
Givanaldo <givanaldo@gmail.com>
2023-06-19 12:02:03 -0300

    atualizações

---
+++ exercicio02.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Salario: R$ 6500.00\n";
+    cout << "Aumento: R$ " << 6500 + 6500*0.05;
+    return 0;
+}
---
+++ exercicio03.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int raio = 15;
+    float pi = 3.14;
+    float area = pi * raio * raio;
+    cout << "Area = " << area;
+    return 0;
+}

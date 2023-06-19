d89dea0ee7c4398387516e2862ed1b44f618aabe
Givanaldo <givanaldo@gmail.com>
2023-06-19 11:21:10 -0300

    atualização

--- exercicio01.cpp
+++ exercicio01.cpp
@@ -1,1 +1,10 @@
 #include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Nome: Fulano Dital da Silva \n";
+    cout << "Matricula: 12345 \n";
+    cout << "E-mail: fulano.silva@escolar.ifrn.edu.br \n";
+    cout << "Curso: Mecatronica";
+    return 0;
+}

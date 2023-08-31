64c4f1a316822325702727507526b798351aa3c4
Givanaldo <givanaldo@gmail.com>
2023-08-31 09:52:03 -0300

    exemplos

--- exemplo-if-04.cpp
+++ exemplo-if-04.cpp
@@ -13,6 +13,8 @@ int main() {
     else {
         x1 = (-b + sqrt(delta)) / 2*a;
         x2 = (-b - sqrt(delta)) / 2*a;
+        cout << "x1 = " << x1 << endl;
+        cout << "x2 = " << x2 << endl;
     }
     return 0;
 }

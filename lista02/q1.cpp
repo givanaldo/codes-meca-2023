f06376859421778cac17f154af25a6a748aa967d
givanaldo <givanaldo@gmail.com>
2023-10-04 07:48:45 -0300

    lista 2 q1

--- lista02/q1.cpp
+++ lista02/q1.cpp
@@ -1,3 +1,19 @@
-// a < b + c
-// b < a + c 
-// c < a + b
+// a < b + c # b < a + c # c < a + b
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b, c;
+    cout << "Lados do triangulo: ";
+    cin >> a >> b >> c;
+    if (a < b+c && b < a+c && c < a+b) {
+        if (a == b && b == c)
+            cout << "Triangulo Equilatero";
+        else if (a == b || a == c || b == c)
+            cout << "Triangulo Isoceles";
+        else
+            cout << "Triangulo Escaleno";
+    }
+    else
+        cout << "Os lados nao formam triangulo.";
+    return 0;
+}

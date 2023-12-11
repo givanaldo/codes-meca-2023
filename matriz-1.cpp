fe7666db626b98e7a8731183557fc1ccf3737e5a
Givanaldo <givanaldo@gmail.com>
2023-12-11 11:37:35 -0300

    exemplo matriz

---
+++ matriz-1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int lin = 2, col = 3;
+    int matriz[lin][col];
+    for (int i=0; i<lin; i++) {
+        for (int j=0; j<col; j++) {
+            cout << "M[" << i << ", " << j << "]: ";
+            cin >> matriz[i][j];
+        }
+    }
+    for (int i=0; i<lin; i++) {
+        for (int j=0; j<col; j++) {
+            cout << matriz[i][j] << " ";
+        }
+    }
+
+    return 0;
+}
